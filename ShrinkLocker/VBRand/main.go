18fa33fa7d81b021d4a849bf183cab3fe63b21fb
Ransomware Uncovered <135552135+facct-ransomware@users.noreply.github.com>
2024-05-30 11:47:10 +0300

    Add files via upload

---
+++ ShrinkLocker/VBRand/main.go
@@ -0,0 +1,73 @@
+package main
+
+
+import (
+  "os"
+  "math"
+  "fmt"
+)
+
+
+const (
+  PasswordChars string = "0123456789thequickbrownfoxjumpsoverthlazydogTHEQUICKBROWNFOXJUMPSOVERTHELAZYDOG!@#$&*-+=_;"
+  PasswordLen = 64
+)
+
+
+// Main
+func main() {
+
+  err := SavePasswords("passwords.txt")
+  if err != nil {
+    fmt.Println("Error: Failed to save passwords")
+    os.Exit(1)
+  }
+}
+
+
+// Save password list
+func SavePasswords(fileName string) error {
+
+  f, err := os.Create(fileName)
+  if err != nil { return err }
+  defer f.Close()
+
+  for i := 0; i <= 0xFFFF; i++ {
+    seed := uint32(i) << 8
+    pwd := GenPassword(seed)
+    _, err = f.Write([]byte(pwd + "\n"))
+    if err != nil { return err }
+  }
+
+  return nil
+}
+
+
+// Generate password
+func GenPassword(seed uint32) string {
+
+  var pwd [PasswordLen]byte
+  var rnd float32
+
+  for i := 0; i < PasswordLen; i++ {
+    rnd, seed = VBRnd(seed)
+    pwd[i] = PasswordChars[int(rnd * float32(len(PasswordChars)))]
+  }
+  return string(pwd[:])
+}
+
+
+// Visual Basic Randomize
+func VBRandomize(seed float64) uint32 {
+
+  n := uint32(math.Float64bits(seed) >> 32)
+  return ((n << 8) ^ (n >> 8)) & 0xFFFF00
+}
+
+
+// Visual Basic Rnd
+func VBRnd(seed uint32) (float32, uint32) {
+
+  seed = (0xFFC39EC3 - seed * 0x2BC03) & 0xFFFFFF
+  return (float32(seed) * float32(5.9604645e-8)), seed
+}
