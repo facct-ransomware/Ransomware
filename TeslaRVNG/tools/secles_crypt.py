eeee709c1a737450357d34eb2d6c30accc9090ec
Ransomware Uncovered <135552135+facct-ransomware@users.noreply.github.com>
2024-09-08 02:46:14 +0300

    Update secles_crypt.py

--- TeslaRVNG/tools/secles_crypt.py
+++ TeslaRVNG/tools/secles_crypt.py
@@ -41,6 +41,16 @@ S_MARKER = b'\x2D\1'
 O_MARKER = b'\x56\1'
 
 
+# Session key data
+SESSION_KEYDATA_MARKER1 = b'>^'
+SESSION_KEYDATA_MARKER2 = b'<~'
+SESSION_KEYDATA_MARKER3 = b':A6N'
+SESSION_KEYDATA_MARKER4 = b'Z5,'
+
+VICTIM_ID_LEN = 20
+SESSION_KEY_INDEX_SIZE = 4
+
+
 # x25519
 X25519_KEY_SIZE = 32
 
@@ -54,6 +64,13 @@ MAC_TAG_SIZE = 16
 CHACHA_KEY_SIZE = 32
 CHACHA_NONCE_SIZE = 8
 CHACHA_KEYDATA_SIZE = CHACHA_KEY_SIZE + CHACHA_NONCE_SIZE
+
+
+MIN_SESSION_KEYDATA_SIZE = (len(SESSION_KEYDATA_MARKER1) + VICTIM_ID_LEN +
+                            len(SESSION_KEYDATA_MARKER2) +
+                            SESSION_KEY_INDEX_SIZE +
+                            len(SESSION_KEYDATA_MARKER3) + X25519_KEY_SIZE +
+                            len(SESSION_KEYDATA_MARKER4))
 
 
 def rsa_decrypt(enc_data: bytes, priv_key: RSA.RsaKey) -> bytes:
@@ -141,6 +158,96 @@ def decrypt_session_key_data(enc_session_key_data: bytes,
     return chacha20_decrypt(enc_data2, chacha_key, chacha_nonce)
 
 
+def parse_session_key_data(
+    session_key_data: bytes,
+    pos: int = 0) -> tuple[bytes, int, bytes, bytes] | None:
+    """Parse session key data"""
+
+    if pos + MIN_SESSION_KEYDATA_SIZE >= len(session_key_data):
+        return None
+
+    p2 = pos + len(SESSION_KEYDATA_MARKER1) + VICTIM_ID_LEN
+    p3 = p2 + len(SESSION_KEYDATA_MARKER2) + SESSION_KEY_INDEX_SIZE
+    p4 = p3 + len(SESSION_KEYDATA_MARKER3) + X25519_KEY_SIZE
+
+    # Check markers
+    m1 = session_key_data[pos : pos + len(SESSION_KEYDATA_MARKER1)]
+    m2 = session_key_data[p2 : p2 + len(SESSION_KEYDATA_MARKER2)]
+    m3 = session_key_data[p3 : p3 + len(SESSION_KEYDATA_MARKER3)]
+    m4 = session_key_data[p4 : p4 + len(SESSION_KEYDATA_MARKER4)]
+    if ((m1 != SESSION_KEYDATA_MARKER1) or
+        (m2 != SESSION_KEYDATA_MARKER2) or
+        (m3 != SESSION_KEYDATA_MARKER3) or
+        (m4 != SESSION_KEYDATA_MARKER4)):
+        return None
+
+    victim_id = session_key_data[pos + len(SESSION_KEYDATA_MARKER1) : p2]
+    key_index_data = session_key_data[p2 + len(SESSION_KEYDATA_MARKER2) : p3]
+    key_index = int.from_bytes(key_index_data, byteorder='little')
+    ecc_key = session_key_data[p3 + len(SESSION_KEYDATA_MARKER3) : p4]
+    rsa_key = session_key_data[p4 + len(SESSION_KEYDATA_MARKER4):]
+    return victim_id, key_index, ecc_key, rsa_key
+
+
+def extract_s_session_keys(
+    enc_data: bytes,
+    rsa_priv_key_data: bytes,
+    ecc_priv_key_data: bytes) -> list[bytes, int, bytes, bytes]:
+    """Extract session keys from s-file"""
+
+    # Decrypt session key data
+    session_key_data = decrypt_session_key_data(enc_session_key_data,
+                                                rsa_priv_key_data,
+                                                ecc_priv_key_data)
+    if not session_key_data:
+        return []
+
+    # Parse session key data
+    session_keys = parse_session_key_data(session_key_data)
+    if not session_keys:
+        return []
+
+    return [session_keys]
+
+
+def extract_o_session_keys(
+    enc_data: bytes,
+    rsa_priv_key_data: bytes,
+    ecc_priv_key_data: bytes) -> list[bytes, int, bytes, bytes]:
+    """Extract session keys from o-file"""
+
+    session_key_list = []
+
+    while True:
+
+        # Decrypt session key data
+        session_key_data = decrypt_session_key_data(enc_data,
+                                                    rsa_priv_key_data,
+                                                    ecc_priv_key_data)
+        if not session_key_data:
+            break
+
+        pos = len(session_key_data)
+
+        while True:
+
+            # Find marker 1
+            pos = session_key_data.rfind(SESSION_KEYDATA_MARKER1, 0, pos)
+            if pos < 0:
+                return session_key_list
+
+            # Parse session key data
+            if pos + MIN_SESSION_KEYDATA_SIZE >= len(session_key_data):
+                continue
+            session_keys = parse_session_key_data(session_key_data, pos)
+            if session_keys:
+                session_key_list.append(session_keys)
+                enc_data = session_key_data[:pos]
+                break
+
+    return session_key_list
+
+
 if __name__ == '__main__':
     #
     # Main
@@ -148,7 +255,18 @@ if __name__ == '__main__':
     import sys
     import io
     import os
-    import base64
+    import errno
+
+
+    def mkdirs(dir):
+        """Create directory hierarchy"""
+
+        try:
+            os.makedirs(dir)
+
+        except OSError as exception:
+            if (exception.errno != errno.EEXIST):
+                raise
 
     if len(sys.argv) != 2:
         print('Usage:', os.path.basename(sys.argv[0]), 'filename')
@@ -171,28 +289,37 @@ if __name__ == '__main__':
 
     # Check file marker
     marker = enc_session_key_data[-MARKER_SIZE:]
-    if marker == S_MARKER:
-        # s-file
-        m_rsa_priv_key_data = m_rsa_priv_key_data0
-        m_ecc_priv_key_data = m_ecc_priv_key_data0
-    elif marker == O_MARKER:
-        # o-file
-        m_rsa_priv_key_data = m_rsa_priv_key_data1
-        m_ecc_priv_key_data = m_ecc_priv_key_data1
-    else:
+    if (marker != S_MARKER) and (marker != O_MARKER):
         print('Error: Invalid file marker')
         sys.exit(1)
 
     enc_session_key_data = enc_session_key_data[:-MARKER_SIZE]
 
-    # Decrypt session key data
-    session_key_data = decrypt_session_key_data(enc_session_key_data,
-                                                m_rsa_priv_key_data,
-                                                m_ecc_priv_key_data)
-    if not session_key_data:
-        print('Error: Failed to decrypt session key data')
+    if marker == S_MARKER:
+        # s-file
+        session_key_list = extract_s_session_keys(enc_session_key_data,
+                                                  m_rsa_priv_key_data0,
+                                                  m_ecc_priv_key_data0)
+    else:
+        # o-file
+        session_key_list = extract_o_session_keys(enc_session_key_data,
+                                                  m_rsa_priv_key_data1,
+                                                  m_ecc_priv_key_data1)
+
+    if not session_key_list:
+        print('Error: Failed to decrypt session keys')
         sys.exit(1)
 
-    new_filename = filename + '.dec'
-    with io.open(new_filename, 'wb') as f:
-        f.write(session_key_data)
+    dest_dir = filename + '.keys/'
+    mkdirs(dest_dir)
+
+    for (victim_id, key_index, ecc_key, rsa_key) in session_key_list:
+
+        key_filename = '%s_%08X_' % (victim_id.decode(), key_index)
+        key_filepath = dest_dir + key_filename
+        # Save X25519 private key
+        with io.open(key_filepath + 'secc_privkey.bin', 'wb') as f:
+            f.write(ecc_key)
+        # Save RSA private key
+        with io.open(key_filepath + 'srsa_privkey.bin', 'wb') as f:
+            f.write(rsa_key)
